2e4a65f0ba71fd3c20e3c8c7c3d7062b40e567ce
GaoJianXin520 <1603452601@qq.com>
2021-02-01 15:36:59 +0800

    init pdf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <span @click="goPDF()">go pdf</span>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    goPDF() {
+      this.$router.push({name: 'pdf'});
+    }
   }
 }
 </script>
---
+++ src/views/pdf.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <iframe :src="`/pdf/web/viewer.html?file=${url}`" frameborder="0" width="100%" height="600"></iframe>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'pdf',
+  data() {
+    return {
+      url: 'http://localhost:8080/protocol.pdf',
+    }
+  }
+}
+</script>
